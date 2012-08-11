b758d8eb8becdddc406d66f7a18f50a073a1b8bc
Tom Burton <tomburton@Raoul-Duke.local>
2012-08-11 14:58:56 +0100

    reset DB on heroku

--- Gemfile
+++ Gemfile
@@ -4,4 +4,3 @@ gem 'sinatra'
 gem 'data_mapper'
 gem 'httpclient'
 gem 'dm-postgres-adapter', :group => :production
-gem 'do_postgres', :group => :production 
--- guff.rb
+++ guff.rb
@@ -3,8 +3,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 require 'data_mapper'
 require 'json'
-
-#require_relative 'cloud_message_client.rb'
+require_relative 'cloud_message_client.rb'
 
 configure :development do
   enable :logging, :dump_errors, :raise_errors
@@ -30,24 +29,24 @@ class Message
   property :token_id, String, :length => 250
 end
 
-# class Location
-#   include DataMapper::Resource
-# 
-#   property :id, Serial
-#   property :token_id, String, :length => 250
-#   property :accuracy, String
-#   property :latitude, Float
-#   property :longitude, Float
-#   property :created_at, DateTime
-#   property :updated_at, DateTime
-# end
+class Location
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :token_id, String, :length => 250
+  property :accuracy, String
+  property :latitude, Float
+  property :longitude, Float
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 get '/messages/:latitude/:longitude' do
   puts "message called"
-  #expiry = Time.now - 7200
+  expiry = Time.now - 7200
   #old mysql query
   #puts "query: SELECT ((acos( cos( radians(#{params[:latitude]}) ) * cos( radians( a.latitude ) ) * cos( radians( a.longitude ) - radians(#{params[:longitude]}) ) + sin( radians(#{params[:latitude]}) ) * sin( radians( a.latitude ) ) )) * 6371) as distance, a.* FROM messages a WHERE created_at > '#{expiry.strftime('%Y-%m-%d %H:%M:%S')}' HAVING distance < 0.2"
   puts "select id, distance, message, latitude, longitude, created_at from ( select id, message, latitude, longitude, created_at, ( 6371 * acos( cos( radians(#{params[:latitude]}) ) * cos( radians( a.latitude ) ) * cos( radians( a.longitude ) - radians(#{params[:longitude]}) ) + sin( radians(#{params[:latitude]}) ) * sin( radians( a.latitude ) ) ) ) as distance, a.* from messages a ) as dt where distance < 0.2 and created_at > '#{expiry.strftime('%Y-%m-%d %H:%M:%S')}' order by distance asc"
@@ -76,32 +75,32 @@ post '/send' do
   puts "params: " + params.inspect
 
   # Notify other people
-  #@location = Location.first(:token_id => params[:tokenID])
-  #if @location.nil?
+  @location = Location.first(:token_id => params[:tokenID])
+  if @location.nil?
     #Create
-    # @location = Location.create(
-    #     :latitude       => params[:latitude],
-    #     :longitude       => params[:longitude],
-    #     :token_id       => params[:tokenID],
-    #     :created_at => Time.now,
-    #     :updated_at => Time.now
-    #   )
-    # else
-    #   #Update
-    #   @location.latitude = params[:latitude]
-    #   @location.longitude = params[:longitude]
-    #   @location.updated_at = Time.now
-    # end 
-  #@location.save
+    @location = Location.create(
+      :latitude       => params[:latitude],
+      :longitude       => params[:longitude],
+      :token_id       => params[:tokenID],
+      :created_at => Time.now,
+      :updated_at => Time.now
+    )
+  else
+    #Update
+    @location.latitude = params[:latitude]
+    @location.longitude = params[:longitude]
+    @location.updated_at = Time.now
+  end 
+  @location.save
 
   # Now find all nearyby peeps and shout out to them
-  #expiry = Time.now - 7200
-  #@peeps = repository(:default).adapter.select("select token_id from ( select ( 6371 * acos( cos( radians(#{params[:latitude]}) ) * cos( radians( a.latitude ) ) * cos( radians( a.longitude ) - radians(#{params[:longitude]}) ) + sin( radians(#{params[:latitude]}) ) * sin( radians( a.latitude ) ) ) ) as distance, a.* from locations a ) as dt where distance < 0.2 and created_at > '#{expiry.strftime('%Y-%m-%d %H:%M:%S')}' and token_id!='#{params[:tokenID]}' order by created_at desc")
+  expiry = Time.now - 7200
+  @peeps = repository(:default).adapter.select("select token_id from ( select ( 6371 * acos( cos( radians(#{params[:latitude]}) ) * cos( radians( a.latitude ) ) * cos( radians( a.longitude ) - radians(#{params[:longitude]}) ) + sin( radians(#{params[:latitude]}) ) * sin( radians( a.latitude ) ) ) ) as distance, a.* from locations a ) as dt where distance < 0.2 and created_at > '#{expiry.strftime('%Y-%m-%d %H:%M:%S')}' and token_id!='#{params[:tokenID]}' order by created_at desc")
 
-  #puts "Number of peeps to push to #{@peeps.length}"
+  puts "Number of peeps to push to #{@peeps.length}"
 
 
 
-  #CloudMessageClient::sendMessage(@peeps, params[:message])
+  CloudMessageClient::sendMessage(@peeps, params[:message])
 
 end
